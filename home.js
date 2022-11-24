5bb98c151ce97f491afc4251a630011223321893
Kaushal Joshi <kaushalpjoshi9@gmail.comgit config --global user.name Kaushal>
2022-11-24 11:22:21 +0530

    code

---
+++ home.js
@@ -0,0 +1,9 @@
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('coupon').style.opacity='0.8';
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('coupon').style.opacity='0.9';
+}
